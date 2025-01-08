2bb7bbd1d9f3e9a0ca11e6735dc10993e9d04aaf
botelllhx <maat9779@gmail.com>
2025-01-07 21:32:26 -0300

    salvar

--- receitas/views.py
+++ receitas/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'receitas/home.html')
 
 def sobre(request):
     return HttpResponse('SOBRE')
