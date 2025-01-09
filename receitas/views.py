440a8c18223c71e26b1e9ede6755e23c3d0d5279
botelllhx <maat9779@gmail.com>
2025-01-09 13:04:03 -0300

    URLS no Django

--- receitas/views.py
+++ receitas/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'receitas/pages/home.html')
+
+def receita(request):
+    return render(request, 'receitas/pages/home.html')
