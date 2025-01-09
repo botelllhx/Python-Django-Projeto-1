638e1394162f6dfa0a45bcb7efc91da023f9a7c3
botelllhx <maat9779@gmail.com>
2025-01-09 18:11:36 -0300

    Detail Page e List Page

--- receitas/views.py
+++ receitas/views.py
@@ -5,12 +5,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'receitas/pages/home.html', context={
-        'name': 'Mateus Botelho',
         'recipes': [make_recipe() for _ in range(10)],
     })
 
 def receita(request, id):
    return render(request, 'receitas/pages/recipe-view.html', context={
-        'name': 'Mateus Botelho',
         'recipe': make_recipe(),
+        'is_detail_page':True,
     })
